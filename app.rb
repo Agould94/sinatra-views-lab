18b451a5ad261619828a6a9f88a3110ecd156a24
Alexander Gould <alexander.gould94@gmail.com>
2021-04-27 14:57:03 -0700

    commit tree 3171361622b90a218efbec1ca972263eca38cb0c

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 class App < Sinatra::Base
+	set :views, settings.root + '/4:26-4:30/sinatra-views-lab/views'
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
 	end
 
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 
 end
